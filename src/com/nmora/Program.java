155c514baa8db30ed64c790b4cf614f9ffaafacb
N-Mora <mora.nicolas94@gmail.com>
2018-03-19 16:10:03 +0100

    Rajoute une exception.

--- src/com/nmora/Program.java
+++ src/com/nmora/Program.java
@@ -1,10 +1,17 @@
 package com.nmora;
 
+import java.io.File;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		try {
+			File.createTempFile("", null);
+		} catch (Exception erreur) {
+			erreur.printStackTrace();
+		}
 	}
 
 }
